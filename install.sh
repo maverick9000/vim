6bfabbdd59c325a739b88b9a57d1b8937ff8b33c
maverick <maverick>
2019-05-16 09:11:35 +0900

    copy instead of link because cleaning up after

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ git clone https://github.com/maverick9000/vim
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-ln -s $SCRIPTPATH/vimrc ~/.vimrc
+cp $SCRIPTPATH/vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo 'run this inside vim => :PlugInstall'
